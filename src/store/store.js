86a4f20384bcfa3ed171b51d8a24dbc876b85160
wangxinwei18 <wangxinwei18@hotmail.com>
2023-07-28 17:55:22 +0800

    finish redux-toolkit section18

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,44 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+// import { compose, createStore, applyMiddleware } from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['user'],
+  blacklist: ["user"],
 };
 
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(middleWares),
+// });
+
+// setting persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleWares),
+});
 
 export const persistor = persistStore(store);
